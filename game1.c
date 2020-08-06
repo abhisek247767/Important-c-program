2ee73573c2e41a70a7af71a51ea0045a3b07ba0f
abhisek247767 <69287212+abhisek247767@users.noreply.github.com>
2020-08-06 10:29:13 +0530

    Add files via upload
    This is a simple game. friends you can try it.

---
+++ game1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+	int choice;
+	printf("Enter Your Favourite Number Between 0 to 5:");
+	scanf("%d",&choice);
+	if(choice==0)
+	printf("I hate you!!");
+		if(choice==1)
+	printf("I miss You!!");
+		if(choice==2)
+	printf("I love you!!");
+		if(choice==3)
+	printf("I really helpful with you!!");
+		if(choice==4)
+	printf("I like you!!");
+		if(choice==5)
+	printf("I Want to help you!!");
+		
+	getch();
+}
