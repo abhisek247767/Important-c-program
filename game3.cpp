4b29d4a47d0d42e1eb534734ec9c0672caecbcc9
abhisek247767 <69287212+abhisek247767@users.noreply.github.com>
2020-08-06 10:31:13 +0530

    Add files via upload
    This is a interesting game. choose your password !!!

---
+++ game3.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+#define ENTER 13
+#define BKSP 8
+#define TAB 9
+#define SPACE 32
+int main()
+{
+	char password[100];
+	int i=0;
+	char ch;
+	printf("Enter password:");
+	while(1)
+	{
+		ch=getch();
+		if(ch==ENTER)
+		{
+			
+		password[i]='\0';
+		break;
+	}
+	else if(ch==BKSP)
+	{
+			if(i>0)
+			{
+				i--;
+				printf("\b \b");
+			}
+	}
+	else if(ch==TAB||ch==SPACE)
+	{
+		continue;
+	}
+	else
+	{
+		password[i]=ch;
+		i++;
+		printf("*");
+		
+	}
+}
+printf("\n\nyour password is %s",password);
+printf("\nlength : %d",i);
+	getch();
+	return 0;
+}
